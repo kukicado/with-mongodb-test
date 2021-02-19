e183333e3345d776b8fb2b858bfe53b16ef30e3e
Ado Kukic <kukicadnan@gmail.com>
2021-02-19 10:35:51 -0800

    Update test.js

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,8 +1,10 @@
 import { connectToDatabase } from '../../util/mongodb'
 export default async function handler(req, res) {
   const { db } = await connectToDatabase();
+  console.log(db);
   
   let data = db.collection("food").find({}).limit(50).toArray();
+  console.log(data);
   
   res.status(200).json(data)
 }
