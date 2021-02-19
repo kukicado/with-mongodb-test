dfbe2b38c52009234d968b14c750d4a16a3261c7
Ado Kukic <kukicadnan@gmail.com>
2021-02-19 10:38:14 -0800

    Update test.js

--- pages/api/test.js
+++ pages/api/test.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   const { db } = await connectToDatabase();
   console.log(db);
   
-  let data = db.collection("food").find({}).limit(50).toArray();
+  let data = await db.collection("food").find({}).limit(50).toArray();
   console.log(data);
   
   res.status(200).json(data)
