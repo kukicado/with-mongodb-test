1aa9c5f59a250de610965431c91422f82db2345e
Ado Kukic <kukicadnan@gmail.com>
2021-02-25 11:11:55 -0800

    Create test3.js

---
+++ pages/api/page/test3.js
@@ -0,0 +1,8 @@
+import { connectToDatabase } from '../../../util/mongodb'
+export default async function handler(req, res) {
+  const { db } = await connectToDatabase();
+  
+  let data = await db.collection("food").find({}).limit(100).toArray();
+  
+  res.status(200).json(data)
+}
